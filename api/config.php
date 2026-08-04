063deb69dfaa69fe285a956bc0add13b191893a9
Rohit Kanekar <kanekarrohit26@gmail.com>
2026-08-04 17:14:47 +0530

    Seperate commit for config.php and product.php changes

--- api/config.php
+++ api/config.php
@@ -3,5 +3,5 @@ define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
 define('SMTP_USER', 'kanekarrohit26@gmail.com');
 define('SMTP_PASS', 'myfp gtsp dyhe fqdx');
-define('MAIL_TO', 'kanekarrohit26@gmail.com');
+define('MAIL_TO', 'concreteartsindia@gmail.com');
 define('MAIL_OWNER', MAIL_TO);
